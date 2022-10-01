b7502481b9387e488c04dde2dbef1a7f12d73255
Ezequielbobbio <ezequielbobbio2001@gmail.com>
2022-10-01 16:31:08 -0300

    Form edition

--- src/Drops/Body/Contact/Contact.js
+++ src/Drops/Body/Contact/Contact.js
@@ -20,26 +20,28 @@ class Contact extends React.Component {
           >
             <BsWhatsapp />
           </a>
-          <div class="mb-3">
+          <div class="mb-3 form-position">
             <label for="exampleFormControlInput1" class="form-label" className='letter-color'>
               Email
             </label>
             <input
               type="email"
-              class="form-control"
+              class="form-control form-size"
               id="exampleFormControlInput1"
               placeholder="nombre@ejemplo.com"
             />
           </div>
-          <div class="mb-3">
+          <div class="mb-3 form-position">
             <label for="exampleFormControlTextarea1" class="form-label" className='letter-color'>
                 Motivo de consulta
             </label>
+            <div className='form-size1'>
             <textarea
               class="form-control"
               id="exampleFormControlTextarea1"
               rows="3"
-            ></textarea>
+            ></textarea> 
+            </div>
             <button type='submit' className='form-button'>Enviar</button>
           </div>
         </div>
